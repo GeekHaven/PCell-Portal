34183c75b110df670181b54c069ce2f0e02ac044
BuddyLongLegs <anurag10jain@gmail.com>
2023-05-03 20:24:10 +0530

    Added Ldap server string in env

--- server/utils/ldap.js
+++ server/utils/ldap.js
@@ -1,21 +1,24 @@
-import {Client} from "ldapts";
-const CONN_STRING = "dc=iiita,dc=ac,dc=in";
+import { Client } from 'ldapts';
+import { config } from 'dotenv';
+config();
 
-const authenticate = async (username, password) => {
+const CONN_STRING = 'dc=iiita,dc=ac,dc=in';
+
+export async function authenticate(username, password) {
   const client = new Client({
-    url: "ldap://172.31.1.41",
+    url: process.env.LDAP_SERVER,
   });
   const options = {
-    filter: "uid=" + username,
+    filter: 'uid=' + username,
   };
   let { searchEntries } = await client.search(CONN_STRING, options);
   let cn = searchEntries[0].dn;
   try {
     await client.bind(cn, password);
-  }catch(e){
+  } catch (e) {
     client.unbind();
     return false;
   }
   client.unbind();
   return true;
-};
+}
