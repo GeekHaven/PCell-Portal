92904442d17c42e1155dde6dc2a2696939014357
Gamma Microwave <pranavt150@gmail.com>
2023-05-06 18:23:54 +0530

    fixed a small issue

--- client/src/pages/admin/users/index.js
+++ client/src/pages/admin/users/index.js
@@ -11,26 +11,7 @@ import { Visibility } from '@mui/icons-material';
 const Users = () => {
   return (
     <>
-      <Container fixed className=" flex flex-col gap-8">
-        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-password"
-          //   type={showPassword ? 'text' : 'password'}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                // onClick={handleClickShowPassword}
-                // onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {<Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-          label="Password"
-        />
-      </Container>
+      <Container fixed className=" flex flex-col gap-8"></Container>
     </>
   );
 };
