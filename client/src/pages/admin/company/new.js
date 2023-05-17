c0105a62f35f66a5eb923922bd568ef9a7aca20b
BuddyLongLegs <anurag10jain@gmail.com>
2023-05-17 07:02:16 +0530

    Fixed api linking from env variable

--- client/src/pages/admin/company/new.js
+++ client/src/pages/admin/company/new.js
@@ -1,1 +1,10 @@
-export default function NewCompany() {}
+import SelectTargets from '@/components/SelectTargets';
+
+export default function NewCompany() {
+  return (
+    <div>
+      <h1>New Company</h1>
+      <SelectTargets />
+    </div>
+  );
+}
