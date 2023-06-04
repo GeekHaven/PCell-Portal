cd8699882510c4a3a455b00cef4a66f39f8487ff
GammaMicrowave <pranavt150@gmail.com>
2023-06-04 19:13:39 +0530

    removed unnecessary comments

--- client/src/utils/API/public/company.js
+++ client/src/utils/API/public/company.js
@@ -2,7 +2,6 @@ import { get } from '../request';
 
 export async function getPublicIndividualCompany(id) {
   let res = await get(`/public/company/${id}`);
-  console.log('sex');
   if (res.status === 200) return Promise.resolve(res.data.data);
   return Promise.reject(res.data.message);
 }
--- server/controllers/public/company.controller.js
+++ server/controllers/public/company.controller.js
@@ -1,4 +1,8 @@
-import { response_200, response_500 } from '../../utils/responseCodes.js';
+import {
+  response_200,
+  response_400,
+  response_500,
+} from '../../utils/responseCodes.js';
 import Company from '../../models/company.model.js';
 import mongoose from 'mongoose';
 
@@ -15,16 +19,10 @@ export const getIndividualCompany = async (req, res) => {
           _id: new mongoose.Types.ObjectId(id),
         },
       },
-      // {
-      //   $addFields: {
-      //     isEligible: isUserEligibleInTarget(req.user),
-      //   },
-      // },
       {
         $project: {
           name: 1,
           logo: 1,
-          // isEligible: 1,
           _id: 1,
           currentStatus: 1,
           hidden: 1,
@@ -36,15 +34,6 @@ export const getIndividualCompany = async (req, res) => {
     if (!companyData) {
       return response_400(res, 'Invalid request');
     }
-    //   if (companyData.isEligible) {
-    //     const companyRelation = await companyUserRelationModel.findOne({
-    //       companyId: id,
-    //       userId: req.user._id,
-    //     });
-    //     companyData.userStatus = companyRelation?.status;
-    //     return response_200(res, 'OK', companyData);
-    //   }
-    //   return response_400(res, 'Invalid request');
     return response_200(res, 'OK', companyData);
   } catch (err) {
     return response_500(res, err);
